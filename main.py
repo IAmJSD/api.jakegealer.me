a8a8c95030d4071bccd1f76d6ade015dd0f4aae5
Jake Gealer <jake@gealer.email>
2019-01-14 17:40:09 +0000

    CORS fix

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from pluginbase import PluginBase
 from flask import Flask
+from flask_cors import CORS
 # Imports go here.
 
 server = Flask(__name__)
 # Defines the web server.
+
+CORS(server)
+# Handles CORS on the server.
 
 plugin_base = PluginBase(package="__main__.plugins")
 plugin_source = plugin_base.make_plugin_source(
